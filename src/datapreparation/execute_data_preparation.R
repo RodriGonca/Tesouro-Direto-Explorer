4259120c05dfccce0cfdfd2e61eaa8f47fbb749f
RodriGonca <rodrigo.goncalves@me.com>
2019-09-15 20:52:33 -0300

    update

--- src/datapreparation/execute_data_preparation.R
+++ src/datapreparation/execute_data_preparation.R
@@ -11,35 +11,9 @@ directoryPath  <- stringr::str_replace(directoryPath, "/markdown", "")
 
 setwd(directoryPath)
 
-
-source(stringr::str_replace(getwd(),
-                            "/markdown",
-                            "/src/datapreparation/step_01_config_environment.R"))
-
-py_run_file(stringr::str_replace(getwd(),
-                            "/markdown",
-                            "/src/datapreparation/step_02_data_download.py"))
-
-source(stringr::str_replace(getwd(),
-                            "/markdown",
-                            "/src/datapreparation/step_03_data_ingestion.R"))
-
-source(stringr::str_replace(getwd(),
-                            "/markdown",
-                            "/src/datapreparation/step_04_data_cleaning.R"))
-
-source(stringr::str_replace(getwd(),
-                            "/markdown",
-                            "/src/datapreparation/step_05_data_enhancement.R"))
-
-source(stringr::str_replace(getwd(),
-                            "/markdown",
-                            "/src/datapreparation/step_06_dataset_preparation.R"))
-
-# 
-# source("./src/datapreparation/step_01_config_environment.R")
-# py_run_file("./src/datapreparation/step_02_data_download.py")
-# source("./src/datapreparation/step_03_data_ingestion.R")
-# source("./src/datapreparation/step_04_data_cleaning.R")
-# source("./src/datapreparation/step_05_data_enhancement.R")
-# source("./src/datapreparation/step_06_dataset_preparation.R")
+source("./src/datapreparation/step_01_config_environment.R")
+py_run_file("./src/datapreparation/step_02_data_download.py")
+source("./src/datapreparation/step_03_data_ingestion.R")
+source("./src/datapreparation/step_04_data_cleaning.R")
+source("./src/datapreparation/step_05_data_enhancement.R")
+source("./src/datapreparation/step_06_dataset_preparation.R")
