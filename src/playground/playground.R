4f210d17f000b75985e85feddfbb309cc7084fb4
Rodrigo <rodrigo.goncalves@me.com>
2021-12-05 21:42:30 -0300

    update

--- src/playground/playground.R
+++ src/playground/playground.R
@@ -1,6 +1,6 @@
 # source scripts --------------------------------------------------------------
 
-source("./src/datapreparation/execute_data_preparation.R")
+# source("./src/datapreparation/execute_data_preparation.R")
 
 # analisys --------------------------------------------------------------------
 
@@ -83,13 +83,13 @@ plot_posfix_trend
 
 ## Histórico da Selic + Inflação ----------------------------------------------
 plot_selic_inflation <- ggplot() +
-  geom_line(data = selic_actual, aes(x = Date, y = Value, 
+  geom_line(data = selic_actual, aes(x = Date, y = Value,
                                      color = 'Selic Annual')) +
   geom_line(data = ipca_12m, aes(x = Date, y = Value, color = 'IPCA 12m')) +
   theme_economist() +
   scale_x_date(date_breaks = '6 month', date_labels = "%b %y") +
   scale_y_continuous(labels = function(x) paste0(x * 100, "%")) +
-  scale_color_manual(values = c('Selic Annual' = 'darkblue', 
+  scale_color_manual(values = c('Selic Annual' = 'darkblue',
                                 'IPCA 12m' = 'darkred'),
                      name = NULL) +
   labs(y = '% Anualizado',
@@ -105,12 +105,29 @@ plot_selic_inflation
 
 library(av)
 
+my_breaks_function <- function(y) seq(floor(min(y)), ceiling(max(y)), by = step)
+
 save_plot <- function(df) {
-  
+
   png(paste('images/Curva de juros da taxa compra de títulos pré fixados em', format(df$dia, '%Y%m%d'), '.png'),
       width = 3840,
       height = 2160)
 
+  if (min(df$taxa_compra) < min_taxa_compra) {
+    min_taxa_compra <<- min(df$taxa_compra)
+    print(paste('min updated to ', min_taxa_compra))
+  }
+
+
+  if (max(df$taxa_compra) > max_taxa_compra) {
+    print(paste('max updated from ', max_taxa_compra))
+    max_taxa_compra <<- max(df$taxa_compra)
+    print(paste('max updated to ', max(df$taxa_compra)))
+  }
+
+  min_taxa_compra = floor(round(min_taxa_compra * 100, 2)) / 100
+  max_taxa_compra = ceiling(round(max_taxa_compra * 100, 2)) / 100
+  
   print(  
     ggplot(data=df) +
       geom_label(aes(
@@ -138,7 +155,11 @@ save_plot <- function(df) {
                   color = 'blue',
                   linetype = 'dotted') +
       scale_x_date(date_labels = "%b-%Y") +
-      scale_y_continuous(labels = function(x) paste0(format(x * 100, nsmall=2), "%")) +
+      scale_y_continuous(labels = scales::percent,
+                         limits = c(min_taxa_compra, max_taxa_compra),
+                         breaks = seq(min_taxa_compra, max_taxa_compra, by = 0.01)
+                         ) +
+      #expand_limits(y=min_taxa_compra) +
       scale_color_manual(values = c('Taxa Compra' = 'red'),
                          name = NULL) +
       labs(y = element_blank(),
@@ -151,34 +172,43 @@ save_plot <- function(df) {
             panel.grid.major = element_blank(), 
             panel.grid.minor = element_blank(),
             panel.background = element_blank(),
-            axis.text.y = element_blank(),
+            axis.text.y = element_text(size=32),
             axis.text = element_text(size=32),
             axis.ticks = element_blank(),
             axis.title = element_blank(),
             plot.title = element_text(size=64, 
                                       hjust = 0.5),
-            strip.text = element_text(size=32))
+            strip.text = element_text(size=32)
+            )
     )
   
   dev.off()
 
 }
 
+classes_filter <- c('NTN-B', 'NTN-B Princ')
+#classes_filter <- c('LTN')
 
-dias <- sort(unique(bond_data$dia), decreasing = TRUE)
-#dias <- dias[dias>="2021-01-01"]
+dias <- sort(unique(bond_data$dia), decreasing = FALSE)
+dias <- dias[dias>="2021-01-01"]
+
+
+df <- filter(bond_data, dia==dia[1], classe %in% classes_filter)
+
+min_taxa_compra = min(df$taxa_compra)
+max_taxa_compra = max(df$taxa_compra)
 
 
 for (dia_unique in dias) {
   
-  df <- filter(bond_data, dia==dia_unique, classe %in% c('LTN', 'NTN-B', 'NTN-B Princ'))
+  df <- filter(bond_data, dia==dia_unique, classe %in% classes_filter)
   df$classe_name = plyr::mapvalues(df$classe, bond_names$classe, bond_names$new_name, warn_missing = FALSE)
-  
+
   save_plot(df)
   
 }
   
 
-av_encode_video(list.files('images/', full.names = TRUE), 
-                framerate = as.integer(length(dias) / 60),
-                output = 'teste.mp4')
+av_encode_video(list.files('images/', full.names = TRUE),
+                framerate = as.integer(length(dias) / 30),
+                output = 'NTN-B 2021.mp4')
